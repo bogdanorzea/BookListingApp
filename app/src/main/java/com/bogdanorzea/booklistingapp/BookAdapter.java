e8247c2a58432df723232974bdff95d0ae513d2c
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-07-04 21:30:30 +0300

    Implement book cover from Internet

--- app/src/main/java/com/bogdanorzea/booklistingapp/BookAdapter.java
+++ app/src/main/java/com/bogdanorzea/booklistingapp/BookAdapter.java
@@ -1,20 +1,54 @@
 package com.bogdanorzea.booklistingapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class BookAdapter extends ArrayAdapter<Book> {
+
+    private static String LOG_TAG = BookAdapter.class.getSimpleName();
     private ArrayList<Book> mBooksArrayList;
+    private LruCache<String, Bitmap> mMemoryCache;
 
     public BookAdapter(Context context, ArrayList<Book> objects) {
         super(context, 0, objects);
         mBooksArrayList = objects;
+
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than number of items.
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+    }
+
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
     }
 
     @Override
@@ -29,15 +63,58 @@ public class BookAdapter extends ArrayAdapter<Book> {
         // Get the book from the list
         final Book currentBook = mBooksArrayList.get(position);
 
-        // Set the Book authors
+        // Set the authors
         TextView authorsTextView = (TextView) listItemView.findViewById(R.id.book_author);
         authorsTextView.setText(currentBook.getAuthors());
 
-        // Set the Book title
+        // Set the title
         TextView titleTextView = (TextView) listItemView.findViewById(R.id.book_title);
         titleTextView.setText(currentBook.getTitle());
 
+        // Set the cover
+        // TODO Try to implement a cache mechanism so that the image won't get downloaded every time
+        String currentBookThumbnailLink = currentBook.getThumbnailLink();
+        ImageView coverImageView = (ImageView) listItemView.findViewById(R.id.book_cover);
+
+        if (!TextUtils.isEmpty(currentBookThumbnailLink)) {
+
+            final Bitmap bitmap = getBitmapFromMemCache(currentBookThumbnailLink);
+            if (bitmap != null) {
+                coverImageView.setImageBitmap(bitmap);
+            } else {
+                coverImageView.setImageResource(R.drawable.loading_cover);
+                new FetchImageTask(coverImageView).execute(currentBookThumbnailLink);
+            }
+        }
+
         // Return the view
         return listItemView;
+    }
+
+    class FetchImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView mCoverImageView;
+
+        public FetchImageTask(ImageView imageView) {
+            mCoverImageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap bitmap = null;
+            try {
+                InputStream in = new java.net.URL(urls[0]).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+                addBitmapToMemoryCache(urls[0], bitmap);
+            } catch (Exception e) {
+                Log.d(LOG_TAG, "Error in downloading cover image", e);
+            }
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            mCoverImageView.setImageBitmap(bitmap);
+        }
     }
 }
--- app/src/main/java/com/bogdanorzea/booklistingapp/MainActivity.java
+++ app/src/main/java/com/bogdanorzea/booklistingapp/MainActivity.java
@@ -24,22 +24,20 @@ public class MainActivity extends AppCompatActivity {
         // Find the book list
         mBookListView = (ListView) findViewById(R.id.book_list);
 
-        new JSONQueryTask().execute();
+        // Construct request url
+        Uri baseUri = Uri.parse(GOOGLE_BOOKS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        // TODO Add user queries and replace spaces with +
+        uriBuilder.appendQueryParameter("q", "android");
+        uriBuilder.appendQueryParameter("maxResults", "10");
+
+        new JSONQueryTask().execute(uriBuilder.toString());
     }
 
     private class JSONQueryTask extends AsyncTask<String, Void, ArrayList<Book>> {
-        private static final String json = "{ \"kind\": \"books#volumes\", \"totalItems\": 796, \"items\": [  {   \"kind\": \"books#volume\",   \"id\": \"KPjmuogFmU0C\",   \"etag\": \"VSIpp1kEQjk\",   \"selfLink\": \"https://www.googleapis.com/books/v1/volumes/KPjmuogFmU0C\",   \"volumeInfo\": {    \"title\": \"Android Apps Entwicklung f\\u00FCr Dummies\",    \"authors\": [     \"Donn Felker\"    ],    \"publisher\": \"John Wiley & Sons\",    \"publishedDate\": \"2011-09-06\",    \"description\": \"Welcher Smartphone-Besitzer hatte nicht schon einmal eine kreative Idee f\\u00FCr eine eigene App? In diesem Buch erfahren Sie, wie Sie Ihre Ideen umsetzen und eigene Apps f\\u00FCr Ihr Android-Smartphone programmieren k\\u00F6nnen. Schritt f\\u00FCr Schritt erkl\\u00E4rt der Autor, wie Sie das kostenlos verf\\u00FCgbare SDK (Self Development Kit) herunterladen, mit der Programmiersoftware Eclipse arbeiten, mit der Programmiersprache Java Android Applikationen programmieren und wie Sie Ihre eigenen Apps sogar auf dem Android Markt verkaufen k\\u00F6nnen. Legen Sie los und entwickeln Sie Ihre ganz pers\\u00F6nlichen Apps!\",    \"industryIdentifiers\": [     {      \"type\": \"ISBN_13\",      \"identifier\": \"9783527707324\"     },     {      \"type\": \"ISBN_10\",      \"identifier\": \"3527707328\"     }    ],    \"readingModes\": {     \"text\": false,     \"image\": true    },    \"pageCount\": 344,    \"printType\": \"BOOK\",    \"categories\": [     \"Computers\"    ],    \"maturityRating\": \"NOT_MATURE\",    \"allowAnonLogging\": false,    \"contentVersion\": \"0.10.10.0.preview.1\",    \"imageLinks\": {     \"smallThumbnail\": \"http://books.google.com/books/content?id=KPjmuogFmU0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api\",     \"thumbnail\": \"http://books.google.com/books/content?id=KPjmuogFmU0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api\"    },    \"language\": \"de\",    \"previewLink\": \"http://books.google.ch/books?id=KPjmuogFmU0C&printsec=frontcover&dq=android&hl=&cd=1&source=gbs_api\",    \"infoLink\": \"http://books.google.ch/books?id=KPjmuogFmU0C&dq=android&hl=&source=gbs_api\",    \"canonicalVolumeLink\": \"https://books.google.com/books/about/Android_Apps_Entwicklung_f%C3%BCr_Dummies.html?hl=&id=KPjmuogFmU0C\"   },   \"saleInfo\": {    \"country\": \"CH\",    \"saleability\": \"NOT_FOR_SALE\",    \"isEbook\": false   },   \"accessInfo\": {    \"country\": \"CH\",    \"viewability\": \"PARTIAL\",    \"embeddable\": true,    \"publicDomain\": false,    \"textToSpeechPermission\": \"ALLOWED\",    \"epub\": {     \"isAvailable\": false    },    \"pdf\": {     \"isAvailable\": false    },    \"webReaderLink\": \"http://play.google.com/books/reader?id=KPjmuogFmU0C&hl=&printsec=frontcover&source=gbs_api\",    \"accessViewStatus\": \"SAMPLE\",    \"quoteSharingAllowed\": false   },   \"searchInfo\": {    \"textSnippet\": \"Welcher Smartphone-Besitzer hatte nicht schon einmal eine kreative Idee f\\u00FCr eine eigene App?\"   }  } ]}";
-
         @Override
         protected ArrayList<Book> doInBackground(String... urls) {
-            Uri baseUri = Uri.parse(GOOGLE_BOOKS_REQUEST_URL);
-            Uri.Builder uriBuilder = baseUri.buildUpon();
-
-            // TODO Add user queries and replace spaces with +
-            uriBuilder.appendQueryParameter("q", "android");
-            uriBuilder.appendQueryParameter("maxResults", "10");
-
-            return Utils.getBookArrayList(uriBuilder.toString());
+            return Utils.getBookArrayList(urls[0]);
         }
 
         @Override
